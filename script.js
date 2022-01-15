732774eae974d2b33a84d8c79fe06140ab2834af
Alex Pereira <alexpnet89@gmail.com>
2022-01-15 13:45:54 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,54 @@
+'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highScore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  // when there is no input
+  if (!guess) {
+    displayMessage('⛔ Digite o número');
+  }
+  // when player wins
+  else if (guess == secretNumber) {
+    document.querySelector('.number').textContent = secretNumber;
+    displayMessage('🎉 Acertou!!!');
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
+  }
+  // When guess is wrong
+  else if (guess != secretNumber) {
+    if (score > 0) {
+      displayMessage(guess > secretNumber ? 'Muito alto' : 'Muito baixo');
+      score--;
+      document.querySelector('.score').textContent = score;
+      document.querySelector('.guess').value = '';
+    } else {
+      displayMessage('💣 Você perdeu');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  displayMessage('Tentando Adivinhar');
+  document.querySelector('.score').textContent = 20;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+});
